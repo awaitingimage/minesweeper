bdad2f8bee76112c3a533ba91c4db59b91536092
Liam Farrell <Liam.Farrell@nology.io>
2019-11-27 14:33:58 +0000

    material ui and form styled

--- src/containers/GameForm/GameForm.tsx
+++ src/containers/GameForm/GameForm.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import styles from './GameForm.module.scss';
 
 export interface GameFormProps {
   numOfRows: number;
@@ -8,47 +11,44 @@ export interface GameFormProps {
   handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
 }
 
-export interface GameFormState {}
-
-class GameForm extends React.Component<GameFormProps, GameFormState> {
-  //state = { :  }
-  render() {
-    return (
-      <form onSubmit={this.props.handleSubmit}>
-        <label htmlFor="rows">Number of rows:</label>
-        <input
-          onChange={this.props.handleChange}
-          id="rows"
-          type="number"
-          min="0"
-          max="50"
-          name="numOfRows"
-          value={this.props.numOfRows}
-        />
-        <label htmlFor="columns">Number of columns:</label>
-        <input
-          onChange={this.props.handleChange}
-          id="columns"
-          type="number"
-          min="0"
-          max="50"
-          name="numOfColumns"
-          value={this.props.numOfColumns}
-        />
-        <label htmlFor="mines">Number of mines:</label>
-        <input
-          onChange={this.props.handleChange}
-          id="mines"
-          type="number"
-          min="0"
-          max={this.props.numOfColumns * this.props.numOfRows}
-          name="numOfMines"
-          value={this.props.numOfMines}
-        />
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
+const GameForm: React.SFC<GameFormProps> = props => {
+  return (
+    <form className={styles.form} onSubmit={props.handleSubmit}>
+      <TextField
+        className={styles.numberInput}
+        label="Rows"
+        type="number"
+        name="numOfRows"
+        value={props.numOfRows}
+        onChange={props.handleChange}
+        inputProps={{ min: '1', max: '50' }}
+        helperText="Number of rows"
+      />
+      <TextField
+        className={styles.numberInput}
+        label="Columns"
+        type="number"
+        name="numOfColumns"
+        value={props.numOfColumns}
+        onChange={props.handleChange}
+        inputProps={{ min: '1', max: '50' }}
+        helperText="Number of columns"
+      />
+      <TextField
+        className={styles.numberInput}
+        label="Mines"
+        type="number"
+        name="numOfMines"
+        value={props.numOfMines}
+        onChange={props.handleChange}
+        inputProps={{ min: '1', max: props.numOfColumns * props.numOfRows }}
+        helperText="Number of mines"
+      />
+      <Button variant="contained" color="primary" type="submit">
+        Start
+      </Button>
+    </form>
+  );
+};
 
 export default GameForm;
